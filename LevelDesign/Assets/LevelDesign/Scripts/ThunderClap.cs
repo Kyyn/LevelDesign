46d9406e7005f46be597cbf7c7b55a655b1e8b9a
DESKTOP-681L4K3\robfe <orobfer@gmail.com>
2017-07-24 19:09:20 +0200

    End_State

---
+++ LevelDesign/Assets/LevelDesign/Scripts/ThunderClap.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThunderClap : MonoBehaviour 
+{
+    public AudioClip clip;
+    bool canFlicker = true;
+    Light _light;
+    AudioSource audioSource;
+	// Use this for initialization
+	void Start () 
+	{
+        _light = GetComponent<Light>();
+        audioSource = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+        StartCoroutine(Flicker());
+	}
+
+    IEnumerator Flicker()
+    {
+        if (canFlicker)
+        {
+            canFlicker = false;
+            audioSource.PlayOneShot(clip);
+            _light.enabled = true;
+
+            yield return new WaitForSeconds(Random.Range(0.1f, 0.4f));
+
+            _light.enabled = false;
+
+            yield return new WaitForSeconds(Random.Range(0.1f, 5.0f));
+
+            canFlicker = true;
+        }
+    }
+}
